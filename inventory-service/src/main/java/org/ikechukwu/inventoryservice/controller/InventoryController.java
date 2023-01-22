11364bafce1f5f794f3905316b42970205b88863
Ikechukwu Iwunna <89745907+Manuel1407@users.noreply.github.com>
2023-01-22 08:55:13 +0100

    inventory controller added

---
+++ inventory-service/src/main/java/org/ikechukwu/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package org.ikechukwu.inventoryservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.ikechukwu.inventoryservice.InventoryServiceApplication;
+import org.ikechukwu.inventoryservice.service.InventoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+        return inventoryService.isInStock(skuCode);
+    }
+}
