174a5e6e64f5e5bdab288137d7527d5a04ac070a
Ikechukwu Iwunna <89745907+Manuel1407@users.noreply.github.com>
2023-04-09 17:34:17 +0100

    implemented timelimiter with resilienc4j

--- inventory-service/src/main/java/org/ikechukwu/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/org/ikechukwu/inventoryservice/service/InventoryService.java
@@ -1,6 +1,8 @@
 package org.ikechukwu.inventoryservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.ikechukwu.inventoryservice.dto.InventoryResponse;
 import org.ikechukwu.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
@@ -13,14 +15,19 @@ import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows                   // Dont use in production code
     public List<InventoryResponse> isInStock(List<String> skuCode) {
-        return inventoryRepository.findBySkuCodeIn(skuCode)
-                .stream().map(inventory -> InventoryResponse.builder()
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory -> InventoryResponse.builder()
                         .skuCode(inventory.getSkuCode())
                         .isInStock(inventory.getQuantity() > 0)
                         .build()
--- order-service/src/main/java/org/ikechukwu/orderservice/controller/OrderController.java
+++ order-service/src/main/java/org/ikechukwu/orderservice/controller/OrderController.java
@@ -1,11 +1,14 @@
 package org.ikechukwu.orderservice.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.ikechukwu.orderservice.dto.OrderRequest;
 import org.ikechukwu.orderservice.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,13 +19,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @CircuitBreaker(name = "Inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
 
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
-        return "Oops! something went wrong, please order after some time!";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return CompletableFuture.supplyAsync(() -> "Oops! something went wrong, please order after some time!");
     }
 }
